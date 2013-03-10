5de26e9658b065a9135548dbeb19e72199c4dad2
Tomasz Matuszewski <tomasumat@gmail.com>
2013-03-10 17:57:28 +0100

    RAVEN - add hello world controller to test spring behaviour

---
+++ raven-ui/src/main/java/org/raven/ui/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package org.raven.ui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController
+{
+	@RequestMapping("/HelloWorld/")
+	public String printHelloWorld(Model model)
+	{
+		model.addAttribute("message", "Hello world");
+		return "hello World";
+	}
+
+}
