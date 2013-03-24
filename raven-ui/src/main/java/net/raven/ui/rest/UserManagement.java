c216183a0856da1c3fe1c31615eadef955624aa6
Tomasz Matuszewski <tomasumat@gmail.com>
2013-03-25 00:05:14 +0100

    RAVEN - add post and get methods working with hibernate

--- raven-ui/src/main/java/net/raven/ui/rest/UserManagement.java
+++ raven-ui/src/main/java/net/raven/ui/rest/UserManagement.java
@@ -1,21 +1,57 @@
 package net.raven.ui.rest;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
+import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import net.raven.core.dao.user.UserManager;
+import net.raven.core.entity.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@Path("/payment")
+@Path("/")
 public class UserManagement
 {
-	@GET
-	@Path("/rest")
-	public Response savePayment()
-	{
-		return Response.status(200).build();
+	private final UserManager userManager;
 
+	@Autowired
+	public UserManagement(UserManager userManager)
+	{
+		this.userManager = userManager;
 	}
 
+	@GET
+	@Path("/user")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getUser()
+	{
+		List<User> list = userManager.getUserList();
+		System.out.println();
+		return Response.status(200).entity(list.get(0)).build();
+	}
+
+	@POST
+	@Path("/user")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response addUser(@FormParam("login") final String login)
+	{
+		User newUser = new User();
+		if (login == null || login.length() == 0)
+		{
+			return Response.status(Status.NO_CONTENT).build();
+		}
+		newUser.setLogin(login);
+		userManager.addUser(newUser);
+		return Response.status(200).build();
+	}
 }
